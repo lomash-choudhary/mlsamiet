dc88635fb3adfba10627ad356fffc3cab2f03d43
Dev Shakya <devxoshakya@gmail.com>
2024-12-14 15:20:32 +0530

    css fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
-/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 
 
 export default {
@@ -59,5 +60,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
